d76c2a2a2b75120f47604ab948d91683e2fa97f5
momadAB <mabaqer99@gmail.com>
2024-11-11 09:13:11 +0300

    Done

--- src/main/java/com/example/demo/RController.java
+++ src/main/java/com/example/demo/RController.java
@@ -3,39 +3,35 @@ package com.example.demo;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class RController {
   private final List<ContactJSON> contacts = new ArrayList<>();
 
   @PostMapping("/addContact")
-  public String addContact(@RequestBody ContactJSON inputContactJSON) {
+  public ResponseEntity<String> addContact(@RequestBody ContactJSON inputContactJSON) {
     // Check if contact already exists
     for (ContactJSON contact : contacts) {
       if (contact.getEmail().equals(inputContactJSON.getEmail())) {
-        return "Contact already exists with this email!";
+        return ResponseEntity.status(HttpStatus.CONFLICT).body("Contact already exists with this email!");
       }
     }
     // If contact does not already exist, add it
     contacts.add(inputContactJSON);
-    return "Contact added successfully!";
+    return ResponseEntity.status(HttpStatus.CREATED).body("Contact added successfully!");
   }
 
   @GetMapping("/getContactDetails")
-  public ContactJSON getContactDetails(@RequestParam String name) {
+  public ResponseEntity<ContactJSON> getContactDetails(@RequestParam String name) {
     // Check if contact exists and return JSON if it does
-
     for (ContactJSON contact : contacts) {
       if (contact.getName().equals(name)) {
-        return contact;
+        return ResponseEntity.status(HttpStatus.OK).body(contact);
       }
     }
     // Return error message if contact does not exist
-    // I assume that the message would reveal too much information and be insecure,
-    //  but its just for proof of concept.
-    throw new ResponseStatusException(
-        HttpStatus.NOT_FOUND, "Contact with name '" + name + "' not found.");
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
   }
 }
